f4a74be2e2096fab6e4bebb600fb5970109e07a1
lit-kei <puyoteto123@yahoo.co.jp>
2025-05-28 16:49:09 +0900

    update

--- test.js
+++ test.js
@@ -8,18 +8,32 @@ let id = 0;
 let len = 0;
 
 
+// 現在のURLのクエリパラメータを取得する場合
+const params = (new URLSearchParams(window.location.search)).get("f");
 
 const fileContent = localStorage.getItem('fileContent');
 const fileName = localStorage.getItem('fileName');
 document.getElementById('fileName').textContent = fileName;
 try {
-    // JSONとしてパースして配列に変換
-    dataArray = JSON.parse(fileContent);
-    question = JSON.parse(fileContent);
+    setArray();
+    question = [...dataArray];
     shuffleArray(question);
     init();
 } catch (error) {
     console.error('ファイルの内容がJSONとしてパースできません:', error);
+}
+
+function setArray() {
+    switch (params) {
+        case 'pp':
+            dataArray = [["～である be","be - was - been"],["～させる let","let - let - let"],["支える bear","bear - bore - borne"],["横たわる lie","lie - lay - lain"],["打つ、負かす beat","beat - beat - beaten"],["なくす lose","lose - lost - lost"],["～になる become","become - became - become"],["作る make","make - made - made"],["始める begin","begin - began - begun"],["意味する mean","mean - meant - meant"],["噛む bite","bite - bit - bitten"],["会う meet","meet - met - met"],["吹く blow","blow - blew - blown"],["追い越す overtake","overtake - overtook - overtaken"],["壊す break","break - broke - broken"],["支払う pay","pay - paid - paid"],["持ってくる bring","bring - brought - brought"],["置く put","put - put - put"],["建てる build","build - built - built"],["読む read","read - read - read"],["燃やす burn","burn - burnt - burnt"],["乗る ride","ride - rode - ridden"],["買う buy","buy - bought - bought"],["ベルが鳴る ring","ring - rang - rung"],["つかむ catch","catch - caught - caught"],["上がる rise","rise - rose - risen"],["選ぶ choose","choose - chose - chosen"],["走る run","run - ran - run"],["来る come","come - came - come"],["言う　 say","say - said - said"],["費用がかかる cost","cost - cost - cost"],["見る see","see - saw - seen"],["切る cut","cut - cut - cut"],["売る sell","sell - sold - sold"],["掘る dig","dig - dug - dug"],["送る send","send - sent - sent"],["する do","do - did - done"],["置く set","set - set - set"],["描く draw","draw - drew - drawn"],["振る shake","shake - shook - shaken"],["飲む drink","drink - drank - drunk"],["発射する shoot","shoot - shot - shot"],["運転する drive","drive - drove - driven"],["見せる show","show - showed - shown"],["食べる eat","eat - ate - eaten"],["閉じる shut","shut - shut - shut"],["落ちる fall","fall - fell - fallen"],["歌う sing","sing - sang - sung"],["食べ物を与える feed","feed - fed - fed"],["沈む sink","sink - sank - sunk"],["感じる feel","feel - felt - felt"],["座る sit","sit - sat - sat"],["戦う fight","fight - fought - fought"],["眠っている sleep","sleep - slept - slept"],["捜して見つける find","find - found - found"],["話す speak","speak - spoke - spoken"],["忘れる forget","forget - forgot - forgotten, forgot"],["金を使う spend","spend - spent - spent"],["手に入れる get","get - got - gotten"],["広げる spread","spread - spread - spread"],["与える give","give - gave - given"],["立つ stand","stand - stood - stood"],["行く go","go - went - gone"],["盗む steal","steal - stole - stolen"],["育つ grow","grow - grew - grown"],["掃く sweep","sweep - swept - swept"],["掛ける hang","hang - hung - hung"],["泳ぐ swim","swim - swam - swum"],["持つ have","have - had - had"],["持って行く take","take - took - taken"],["聞える hear","hear - heard - heard"],["教える teach","teach - taught - taught"],["打つ hit","hit - hit - hit"],["言う、知らせる tell","tell - told - told"],["手でつかむ hold","hold - held - held"],["思う、考える think","think - thought - thought"],["傷つける hurt","hurt - hurt - hurt"],["投げる throw","throw - threw - thrown"],["保つ keep","keep - kept - kept"],["理解する understand","understand - understood - understood"],["知っている know","know - knew - known"],["目が覚める wake","wake - woke - woken"],["横たえる lay","lay - laid - laid"],["身につける wear","wear - wore - worn"],["導く lead","lead - led - led"],["勝つ win","win - won - won"],["去る leave","leave - left - left"],["書く write","write - wrote - written"],["貸す lend","lend - lent - lent"]];
+            document.getElementById('fileName').textContent = '過去分詞';
+            break;
+    
+        default:
+            dataArray = JSON.parse(fileContent);
+            break;
+    }
 }
 
 function init() {
